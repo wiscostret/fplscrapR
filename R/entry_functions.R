07395d19383a8b6357601194345f396cb8252629
Eivind Hammersmark <eivindhammers@gmail.com>
2020-07-28 21:47:39 +0200

    Revert "Allow multiple IDs"
    This reverts commit d4656e1df0cd1655869adb08f2c0e6a8f6f5abbb.

--- R/entry_functions.R
+++ R/entry_functions.R
@@ -84,17 +84,11 @@ get_entry_picks <- function(entryid = NULL, gw = NULL){
   if(is.null(entryid)) stop("You'll need to input an entry ID, mate.")
   if(is.null(gw)) stop("You'll need to input a gameweek, mate.")
 
-  picks2 <- list()
-  for (i in 1:length(entryid)) {
-    picks <- jsonlite::fromJSON(paste("https://fantasy.premierleague.com/api/entry/",entryid[i],"/event/",gw,"/picks","/",sep=""))
-    if (length(entryid) > 1) {
-      picks2[[as.character(entryid[i])]] <- picks
-    } else {
-      picks2 <- picks
-    }
-  }
+  roundinfo <- get_round_info()
 
-  return(picks2)
+  picks <- jsonlite::fromJSON(paste("https://fantasy.premierleague.com/api/entry/",entryid,"/event/",gw,"/picks","/",sep=""))
+
+  return(picks)
 }
 
 get_entry_captain <- function(entryid = NULL, gw = NULL, player_names = FALSE){
